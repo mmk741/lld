90d6e90a510ad802fdc190a6d8248d64d0ffba8e
Moshahid <moshahidkhan741@gmail.com>
2024-12-25 12:50:05 +0530

    stream added

---
+++ src/main/java/stream/App.java
@@ -0,0 +1,126 @@
+package stream;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class App {
+
+    static List<Employee> employeeList = new ArrayList<>();
+
+
+    public static void main(String[] args) throws Exception {
+
+        EmployeeFactory employeeFactory = new EmployeeFactory();
+        employeeList = employeeFactory.getAllEmployee();
+
+        // TODO
+        // QnA
+//        1. List all distinct project in non-ascending order.
+        System.out.println("************************1************************");
+
+        List<Project> project1 = employeeList.stream().flatMap(emp->emp.getProjects().stream()).distinct()
+                .sorted((p1,p2)->p2.getName().length()-p1.getName().length()).collect(Collectors.toList());
+
+
+
+
+
+
+
+
+
+
+
+
+
+        List<Project> project = employeeList.stream().flatMap(x -> x.getProjects().stream()).distinct()
+                .sorted(Comparator.comparing(Project::getName).reversed())
+                .collect(Collectors.toList());
+        project1.forEach(System.out::println);
+//        2. Print full name of any employee whose firstName starts with ‘A’.
+        System.out.println("************************2************************");
+        List<String>  employeeNAme = employeeList.stream().filter(e -> e.getFirstName().charAt(0) == 'A').
+                map(e -> e.getFirstName() + e.getLastName()).collect(Collectors.toList());
+        employeeNAme.forEach(System.out::println);
+
+
+//
+//        3. List of all employee who joined in year 2023 (year to be extracted from employee id i.e., 1st 4 characters)
+        System.out.println("************************3************************");
+
+        List<Employee> employee = employeeList.stream().filter(x -> x.getId().substring(0,4).
+                equalsIgnoreCase("2023")).collect(Collectors.toList());
+employee.forEach(e->System.out.println(e.toString()));
+//
+//        4. Sort employees based on firstName, for same firstName sort by salary.
+        System.out.println("************************4************************");
+        List<Employee> empNameSalarySort = employeeList.stream().
+                sorted(Comparator.comparing(Employee::getFirstName).thenComparing(Employee::getSalary)).collect(Collectors.toList());
+        empNameSalarySort.forEach(e->System.out.println(e.toString()));
+
+//
+//        5. Print names of all employee with 3rd highest salary. (generalise it for nth highest salary).
+        System.out.println("************************5************************");
+
+
+        Optional<Integer> first = employeeList.stream().map(e->e.getSalary()).distinct().
+                sorted((Integer x1,Integer x2)->x2-x1).skip(2).findFirst();
+        List<Employee> thirdHighSalary = employeeList.stream().filter(e -> e.getSalary() == first.orElse(-1)).collect(Collectors.toList());
+thirdHighSalary.forEach(e->System.out.println(e.toString()));
+
+//
+//        6. Print min salary.
+        System.out.println("************************6************************");
+        Optional<Integer> minSal = employee.stream().map(Employee::getSalary).sorted().findFirst();
+        System.out.println("Min salary : "+minSal.orElse(-1));
+
+
+
+
+//
+//        8. List of people working on more than 2 projects.
+        System.out.println("************************8************************");
+        List<Employee> empMoreThanOneProject = employeeList.stream().filter(x -> x.getProjects().size() > 2).collect(Collectors.toList());
+        empMoreThanOneProject.forEach(e->System.out.println(e.toString()));
+
+
+//
+//        9. Count of total laptops assigned to the employees.
+        System.out.println("************************9************************");
+        long count = employeeList.stream().mapToInt(Employee::getTotalLaptopsAssigned).sum();
+        System.out.println(count);
+
+//
+//        10. Count of all projects with Robert Downey Jr as PM.
+        System.out.println("************************10************************");
+        long robert_downey_jr = employeeList.stream().flatMap(e -> e.getProjects().stream()).distinct().filter(e -> e.getProjectManager().equalsIgnoreCase("Robert Downey Jr")).count();
+        System.out.println(robert_downey_jr);
+//
+//        11. List of all projects with Robert Downey Jr as PM.
+        System.out.println("************************11************************");
+        List<Project> robert_downey_jr1 = employeeList.stream().flatMap(e -> e.getProjects().stream()).
+                distinct().filter(e -> e.getProjectManager().equalsIgnoreCase("Robert Downey Jr")).collect(Collectors.toList());
+robert_downey_jr1.forEach(e->System.out.println(e.toString()));
+
+//
+//        12. List of all people working with Robert Downey Jr.
+        System.out.println("************************12************************");
+        List<Employee> employeesWorkingWithTargetManager = employeeList.stream()
+                .filter(e -> e.getProjects().stream()
+                        .anyMatch(p -> p.getProjectManager().equals("Robert Downey Jr")))
+                .collect(Collectors.toList());
+
+        employeesWorkingWithTargetManager.forEach(e->System.out.println(e.toString()));
+//
+//        13. Create a map based on this data, they key should be the year of joining, and value should be list of all the employees who joined the particular year.
+        System.out.println("************************13************************");
+        Map<String, List<Employee>> collectMap = employeeList.stream().collect(Collectors.groupingBy(e -> e.getId().substring(0, 4)));
+       collectMap.forEach((year,emp)-> System.out.println(year+" :"+emp.toString()));
+
+
+//        14. Create a map based on this data, the key should be year of joining and value should be the count of people joined in that particular year.
+        System.out.println("************************14************************");
+        Map<String, Long> collectMap2 = employeeList.stream().collect(Collectors.groupingBy(e -> e.getId().substring(0, 4), Collectors.counting()));
+        collectMap2.forEach((year,emp)-> System.out.println(year+" :"+emp));
+
+    }
+}
---
+++ src/main/java/stream/StreamConcept.java
@@ -0,0 +1,95 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public  class StreamConcept {
+    public static void main(String[] args) {
+
+        //https://notebook.zohopublic.in/public/notes/74tdod05928d081b5484fbfd5937d4129fa64
+        //Predicate, Function ,Consumer
+//        Stream : 1.create stream
+//                 2. intermediate operation
+//                 3.terminal operation
+        //Stream are mainly usefull when we have bulk of data and want to do parallel process
+
+        //Different ways to create stream
+        //1.stream from collection
+        List<Integer> salaryList= Arrays.asList(11,23,123,233,12);
+        Stream<Integer> stream = salaryList.stream();
+
+        //2. from array
+        Integer[] salaryArray=new Integer[]{1,22,333,123};
+        Stream<Integer> stream1 = Arrays.stream(salaryArray);
+
+        //3. from static method Stream.of
+        Stream<Integer> streamfromStaticMethod = Stream.of(1,2,3,1);
+
+        //4 from streamBuilder
+        Stream<Object> streamFromBuilder = Stream.builder().add(100).add(200).add(800).build();
+
+        //5(startPoint , IncrementLogic,limit)
+        Stream<Integer> streamFromIterator = Stream.iterate(800, (Integer n) -> n + 50).limit(5);
+
+
+        //Intermediate operation : it is a lazy operation
+        //1.filter
+        List<Integer> collectFilter = stream.filter(x -> x > 23).collect(Collectors.toList());
+        System.out.println("Filtered Data: " + collectFilter);
+
+        //2.map use to transform each element
+        //. In Java Streams, once a terminal operation is invoked on a stream, the stream is considered closed, and you cannot perform additional operations on it.
+        List<Integer> collectMap = stream1.map(x -> x + 10).collect(Collectors.toList());
+        System.out.println("Mapped Data: " + collectMap);
+
+        //3.FlatMAp :  use to iterate over each element of complex collection and help to flatten it
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7, 8, 9)
+        );
+
+        // Use flatMap to flatten the list of lists into a single stream of integers
+        Stream<Integer> flattenedStream = listOfLists.stream()
+                .flatMap(List::stream);
+
+        // Print the elements of the flattened stream
+        System.out.println("flattened stream : "+flattenedStream.collect(Collectors.toList()));
+
+        //4.distinct : remove duplicate from array
+
+        List<Integer> collectDistinct = streamfromStaticMethod.distinct().collect(Collectors.toList());
+        System.out.println("distinct element: "+ collectDistinct);
+
+        //5.sorted
+        //6.peek
+        //7.limit
+        //8.skip : it will skip first n element
+        //9.mapToInt : help to work with primitive int data type
+        //when we work with primitive data type and do stream on it we get IntStream in case of int
+        //and not Stream, so when IntStream is collected we get primitive data type
+        //10.mapToLong
+        //11.mapToDouble
+
+
+        //3.Terminal operation
+        //1.forEach: consumer as input
+        //2.toArray()
+        //3.reduce
+        //4.collect
+        //5. min , max
+        //6. count
+        //7. anyMatch : return boolean
+        //8. allMatch , noneMatch
+        //9.findFirst , findAny
+        //10.sum
+
+        //ParallelStream : help to preform operation on stream concurrently taking advantage of multicore cpu
+        //fork join pool implementation in multithreading
+
+
+
+    }
+}
